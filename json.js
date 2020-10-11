b71af8d794dd62246de24af6ca1702ea685a1207
Wilson Ramos <hupcontato@gmail.com>
2020-10-11 19:43:53 -0300

    kh

--- json.js
+++ json.js
@@ -9,35 +9,44 @@ var catalogo = {
                 {
                     'cod': 'COD: 1010',
                     'ft': 'imgs/36.webp'
+                },
+                {
+                    'cod': 'COD: 1011',
+                    'ft': 'imgs/37.webp'
                 }
             ]
         },
         {
-            'nome': 'Polo Masculina Detalhe',
+            'nome': 'Polo Masculina Outro',
             'tamanhos': 'Disponivel nos tamanhos P, M, G, GG',
             'avista': '39,90',
             'aprazo': '42,90',
+            'imgs': [
+                {
+                    'cod': '1',
+                    'ft': 'imgs/5.webp'
+                }
+            ]
         }
     ]
 }
 
-
-catalogo.sessao.map((item) => {
+catalogo.sessao.forEach((p)=> {
     var txt = "<div class='detalhes'>";
 
-    txt += "<h2>" + item.nome + "</h2>";
-    txt += "<p class='descricao'>" + item.tamanhos + "</p>";
-    txt += "<span class='precoVista'>" + item.avista + "</span>";
-    txt += "<span class='precoPrazo'>" + item.aprazo + "</span>";
-    txt += "</div>";
-
-    item.imgs.map((img) => {
+    txt += "<h2>" + p.nome + "</h2>";
+    txt += "<p class='descricao'>" + p.tamanhos + "</p>";
+    txt += "<span class='precoVista'>" + p.avista + "</span>";
+    txt += "<span class='precoPrazo'>" + p.aprazo + "</span>";
+    
+    p.imgs.forEach((i)=> {
         var thumb = "<div class='item'>";
-
-        thumb += "<img src=" + img.ft + "/>";
-        thumb += "<span class='cod'>" + img.cod + "</span>";
+        
+        thumb += "<img src=" + i.ft + "/>";
+        thumb += "<span class='cod'>" + i.cod + "</span>";
         thumb += "</div>";
-        $('section').append(thumb)
+        $('.detalhes').append(thumb)
     })
+    txt += "</div>";
     $('section').append(txt);
 })
