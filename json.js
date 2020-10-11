0ff12ffeac95ed3df81b8701337bcc2f74068634
Wilson Ramos <hupcontato@gmail.com>
2020-10-11 20:32:49 -0300

    volta

--- json.js
+++ json.js
@@ -1,52 +1,54 @@
-var catalogo = {
-    sessao: [
-        {
-            'nome': 'Polo Masculina Detalhe',
-            'tamanhos': 'Disponivel nos tamanhos P, M, G, GG',
-            'avista': '39,90',
-            'aprazo': '42,90',
-            'imgs': [
-                {
-                    'cod': 'COD: 1010',
-                    'ft': 'imgs/36.webp'
-                },
-                {
-                    'cod': 'COD: 1011',
-                    'ft': 'imgs/37.webp'
-                }
-            ]
-        },
-        {
-            'nome': 'Polo Masculina Outro',
-            'tamanhos': 'Disponivel nos tamanhos P, M, G, GG',
-            'avista': '39,90',
-            'aprazo': '42,90',
-            'imgs': [
-                {
-                    'cod': '1',
-                    'ft': 'imgs/5.webp'
-                }
-            ]
-        }
-    ]
-}
+// var catalogo = {
+//     sessao: [
+//         {
+//             'nome': 'Polo Masculina Detalhe',
+//             'tamanhos': 'Disponivel nos tamanhos P, M, G, GG',
+//             'avista': '39,90',
+//             'aprazo': '42,90',
+//             'imgs': [
+//                 {
+//                     'cod': 'COD: 1010',
+//                     'ft': '<img src="imgs/36.webp">'
+//                 },
+//                 {
+//                     'cod': 'COD: 1011',
+//                     'ft': '<img src="imgs/37.webp">'
+//                 }
+//             ]
+//         },
+//         {
+//             'nome': 'Polo Masculina Outr',
+//             'tamanhos': 'Disponivel nos tamanhos P, M, G, GG',
+//             'avista': '39,90',
+//             'aprazo': '42,90',
+//             'imgs': [
+//                 {
+//                     'cod': '1',
+//                     'ft': '<img src="imgs/5.webp">'
+//                 }
+//             ]
+//         }
+//     ]
+// }
 
-catalogo.sessao.forEach((p)=> {
-    var txt = "<div class='detalhes'>";
+// catalogo.sessao.forEach((p)=> {
+//     var prod = "<div class='detalhes'>";
 
-    txt += "<h2>" + p.nome + "</h2>";
-    txt += "<p class='descricao'>" + p.tamanhos + "</p>";
-    txt += "<span class='precoVista'>" + p.avista + "</span>";
-    txt += "<span class='precoPrazo'>" + p.aprazo + "</span>";
+//     prod += "<h2>" + p.nome + "</h2>";
+//     prod += "<p class='descricao'>" + p.tamanhos + "</p>";
+//     prod += "<span class='precoVista'>" + p.avista + "</span>";
+//     prod += "<span class='precoPrazo'>" + p.aprazo + "</span>";
     
-    p.imgs.forEach((i)=> {
-        var thumb = "<div class='item'>";
+//     p.imgs.forEach((i)=> {
+//         var item = "<div class='item'>";
         
-        thumb += "<img src=" + i.ft + "/>";
-        thumb += "<span class='cod'>" + i.cod + "</span>";
-        thumb += "</div>";
-        $('.detalhes').append(thumb)
-    })
-    txt += "</div>";
-    $('section').append(txt);
-})
+//         item += i.ft;
+//         item += "<span class='cod'>" + i.cod + "</span>";
+//         item += "</div>";
+//         $('.detalhes').append(item)
+//     })
+    
+//     prod += "</div>";
+//     $('section').append(prod);
+//     console.log(prod)
+// })
