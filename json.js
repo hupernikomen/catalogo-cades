5a4a91f86fd0d0ef77fcc3a9a3720e9941d2b0e6
Wilson Ramos <hupcontato@gmail.com>
2020-10-11 18:41:02 -0300

    json

---
+++ json.js
@@ -0,0 +1,45 @@
+var catalogo = {
+    sessao: [
+        {
+            'nome': 'Polo Masculina Detalhe',
+            'tamanhos': 'Disponivel nos tamanhos P, M, G, GG',
+            'avista': '39,90',
+            'aprazo': '42,90',
+            'imgs': [
+                {
+                    'cod': 'COD: 1010',
+                    'ft': 'imgs/36.webp'
+                }
+            ]
+        },
+        {
+            'nome': 'Polo Masculina Detalhe',
+            'tamanhos': 'Disponivel nos tamanhos P, M, G, GG',
+            'avista': '39,90',
+            'aprazo': '42,90',
+        }
+    ]
+}
+
+
+catalogo.sessao.map((item) => {
+    var txt = "<div class='detalhes'>";
+
+    txt += "<h2>" + item.nome + "</h2>";
+    txt += "<p class='descricao'>" + item.tamanhos + "</p>";
+    txt += "<span class='precoVista'>" + item.avista + "</span>";
+    txt += "<span class='precoPrazo'>" + item.aprazo + "</span>";
+    txt += "</div>";
+
+    console.log(item.imgs)
+
+    item.imgs.map((img) => {
+        var thumb = "<div class='item'>";
+
+        thumb += "<img src=" + img.ft + "/>";
+        thumb += "<span class='cod'>" + img.cod + "<span>";
+        thumb += "</div>";
+        $('section').append(thumb)
+    })
+    $('section').append(txt);
+})
